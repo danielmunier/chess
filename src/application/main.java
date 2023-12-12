085a1e220885f1ec3083d3a937be3f7cde41e66e
Daniel <dmunier27@gmail.com>
2023-12-12 14:51:20 -0300

    new: First class Position

---
+++ src/application/main.java
@@ -0,0 +1,11 @@
+package application;
+
+import board.Position;
+
+public class main {
+	
+	public static void main(String[] args) {
+		Position pos = new Position(1, 2);
+		System.out.println(pos);
+	}
+}
